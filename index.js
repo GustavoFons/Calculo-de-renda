1db127a128c45eb6780a4eb3f548d6c2f6b822f6
Gustavo <gustavo_fonseca2001@hotmail.com>
2022-06-11 00:31:49 -0300

    Implementação api e fetch

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const axios = require("axios");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 
+app.post("/api/calculoRenda", async (req, res) => {
+    const dados = req.body;
+
+    const endereco = await axios.get(
+       ` https://viacep.com.br/ws/${dados.cep}/json/ `
+       );
+
+    res.send({
+        nome: dados.nome,
+        endereco: endereco.data,
+        renda: dados.renda / dados.dependentes,
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${ PORT }`);
 });
+
--- public/script.js
+++ public/script.js
@@ -4,10 +4,20 @@ if (form.attachEvent) {
 } else {
     form.addEventListener("submit", onsubmit);
 }
-function onsubmit(event) {
+async function onsubmit(event) {
     event.preventDefault();
-    
 
+    const data = new FormData(event.currentTarget);
+    const values = Object.fromEntries(data.entries());
+    const result = await fetch("/api/calculoRenda", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+    }); 
+    const json = await result.json();
+    console.log(json);
 
     return false;
 }
